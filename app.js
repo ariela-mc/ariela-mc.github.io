292f62dd30242ddd91b5481fe2b28081d4d3dd4f
Ariela Mcclame <ariela3500@gmail.com>
2025-07-13 13:28:22 +1200

    Create app.js for BIG UPDATE

---
+++ app.js
@@ -0,0 +1,37 @@
+const sections = [...document.querySelectorAll('section')]
+
+let options = {
+    threshold: 0.5
+}
+
+const handleIntersect = (entries) =>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            document.body.style.backgroundColor = entry.target.getAttribute('data-color')
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+
+sections.forEach(section => {
+    observer.observe(section)
+})
+
+function alreadyHere() {
+    alert("You are already here!");
+}
+
+function doesntExist() {
+    alert("Whoops! This page doesnt exist.")
+}
+
+var togglebtn=document.querySelector(".toggle-btn");
+            var nav=document.querySelector(".nav-links");
+            var links=document.querySelectorAll(".nav-links li");
+            togglebtn.addEventListener("click", function(){
+                this.classList.toggle("click");
+                nav.classList.toggle("open");
+            })
+
+        
